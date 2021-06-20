512218e013946a4363c2c36f1ec999c5ff21677d
Marcel <marcelstreicher@gmail.com>
2021-06-19 22:22:02 -0300

    linkdabio

--- pages/index.js
+++ pages/index.js
@@ -5,47 +5,22 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>BORA PRA BC</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+      <img src="/boraprabc.png" alt="BORA PRA BC" className={styles.logo} />
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href="https://www.youtube.com/channel/UC4hv5oGOGyzTGoUZlWvJDPw" className={styles.card}>
+            <h3>Canal no Youtube &rarr;</h3>
+            <p>O material mais completo você encontrará no nosso canal do Youtube</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+          <a href="https://instagram.com/boraprabc" className={styles.card}>
+            <h3>Instagram &rarr;</h3>
+            <p>Promoções, dicas, fotos e vídeos exclusivos!</p>
           </a>
         </div>
       </main>
@@ -57,7 +32,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          <img src="/boraprabc.png" alt="BORA PRA BC" className={styles.logofooter} />
         </a>
       </footer>
     </div>
